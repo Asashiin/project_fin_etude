ab2434c1b8cf059cd61c4783db74f9958109101d
silouane.galinou <silouane.galinou@efrei.net>
2017-12-23 19:35:26 +0100

    test

--- server/routes/route.js
+++ server/routes/route.js
@@ -2,15 +2,21 @@ const superagent = require('superagent')
 async function testApi(api, res) {
     if (api === 'gwas_api') {
         // Return some information about the API (just for testing)
-        const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api')
+        const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api/singleNucleotidePolymorphisms/rs6538678')
+        let association = {
+            id_rs: data.body.rsId
+            , func_class: data.body.functionalClass
+            , lastUp: data.body.lastUpdateDate
+            , location: data.body.locations[0]
+        }
         res.render('test', {
-            data: JSON.stringify(data.body)
+            data: JSON.stringify(association)
             , name: 'GWAS'
         })
     }
     if (api === 'gtex_api') {
         // Return the version of the api (just for testing)
-        const data = await superagent.get('rest.ensembl.org/info/data').set('Accept', 'application/json')
+        const data = await superagent.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/').set('Accept', 'application/json')
         res.render('test', {
             data: JSON.stringify(data.body)
             , name: 'GTEX'

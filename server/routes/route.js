a5661f2d11715ea73bd7a9f7832b4ad230704951
silouane.galinou <silouane.galinou@efrei.net>
2017-12-23 16:19:48 +0100

    creation branche GWAS

--- server/routes/route.js
+++ server/routes/route.js
@@ -1,21 +1,20 @@
 const superagent = require('superagent')
-
-async function testApi (api, res) {
-  if (api === 'gwas_api') {
-    // Return some information about the API (just for testing)
-    const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api')
-    res.render('test', {
-      data: JSON.stringify(data.body),
-      name: 'GWAS'
-    })
-  }
-  if (api === 'gtex_api') {
-    // Return the version of the api (just for testing)
-    const data = await superagent.get('rest.ensembl.org/info/rest').set('Accept', 'application/json')
-    res.render('test', {
-      data: JSON.stringify(data.body),
-      name: 'GTEX'
-    })
-  }
+async function testApi(api, res) {
+    if (api === 'gwas_api') {
+        // Return some information about the API (just for testing)
+        const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api')
+        res.render('test', {
+            data: JSON.stringify(data.body)
+            , name: 'GWAS'
+        })
+    }
+    if (api === 'gtex_api') {
+        // Return the version of the api (just for testing)
+        const data = await superagent.get('rest.ensembl.org/info/data').set('Accept', 'application/json')
+        res.render('test', {
+            data: JSON.stringify(data.body)
+            , name: 'GTEX'
+        })
+    }
 }
 module.exports.testApi = testApi

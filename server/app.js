b4cec017f18df06aa0846bbbedec412d282be639
silouane <silouane.galinou@efrei.net>
2017-12-26 01:10:16 +0100

    function searchAssociationsByTrait

--- server/app.js
+++ server/app.js
@@ -3,31 +3,35 @@ const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
 const route = require('./routes/route')
-
 const port = process.env.PORT || 3000
-
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'ejs')
-
 app.get('/', (req, res) => {
   res.render('index')
 })
-
 app.post('/', (req, res) => {
   /* // GWAS Catalog REST API
   route.testApi(req.body.gwas_api, res)
   // GTEX REST API
   route.testApi(req.body.gtex_api, res) */
   if (req.body.rs) {
+    console.log("test route" + req.body.rs)
     route.searchRs(req.body.rs, res)
   }
 })
-
+app.post('/asso', (req, res) => {
+  console.log("test routeeee" + req.body.trait)
+  if (req.body.trait) {
+    console.log("test route" + req.body.trait)
+    route.searchAssociationsByTraits(req.body.trait, res)
+  }
+})
 app.get('/test', (req, res) => {
   res.render('test')
 })
-
 app.listen(port, () => {
   console.log('Server is running on port 3000')
 })
--- server/routes/route.js
+++ server/routes/route.js
@@ -2,75 +2,95 @@ const superagent = require('superagent')
 const gtex = 'rest.ensembl.org/variation/human/'
 const gwas = 'www.ebi.ac.uk/gwas/beta/rest/api/singleNucleotidePolymorphisms/'
 async function testApi(api, res) {
-    if (api === 'gwas_api') {
-        // Return some information about the API (just for testing)
-        // const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api') -> Bonne addresse: ebi.ac.uk/gwas/beta/rest/api
-        const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api/singleNucleotidePolymorphisms/rs6538678').set('Accept', 'application/json')
-        res.render('test', {
-            data: JSON.stringify(data.body)
-            , name: 'GWAS'
-        })
-    }
-    if (api === 'gtex_api') {
-        // Return the version of the api (just for testing) faire un attention à la version (soit 37, 38)!
-        // const data = await superagent.get('grch37.rest.ensembl.org/variation/human/rs4646963').set('Accept', 'application/json')
-        const data = await superagent.get('rest.ensembl.org/variation/human/rs6538678').set('Accept', 'application/json') 
-        let result = {
-            name: data.body.name, // gene: geneId.body.genomicContext
-            location: data.body.mappings[0].location
-            , start: data.body.mappings[0].start
-            , end: data.body.mappings[0].end
-            , seq: data.body.mappings[0].seq_region_name
-            , allele: data.body.mappings[0].allele_string
-            , variant: data.body.synonyms
-        }
-        res.render('result', {
-            data: result
-            , name: 'GTEX'
-        })
-    }
-}
-// Reseach function with RS id given by the user
-async function searchRs(rs, res) {    
-    const gt = await superagent.get(gtex + rs).set('Accept', 'application/json')
-    const gw = await superagent.get(gwas + rs).set('Accept', 'application/json')
-    const gwAsso = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api/associations/search/findByRsId?rsId=' + rs).set('Accept','application/json')    
-    let geneList = []
-    let geneNameList = []    
-    let asso = {}
-    for (let i in gwAsso.body._embedded.associations){    
-      asso.snp = gwAsso.body._embedded.associations[i].loci[0].strongestRiskAlleles[0].riskAlleleName
-      asso.pvalue = gwAsso.body._embedded.associations[i].pvalue     
-    }
-    console.log('test_snp : ' + asso.snp)
-    console.log('test_pvalue : ' + asso.pvalue)
-    for (let i in gw.body.genomicContexts) {
-        if (typeof (gw.body.genomicContexts[i].gene.geneName) !== 'undefined') {           
-            geneNameList.push(gw.body.genomicContexts[i].gene.geneName)
-        }
-        if (typeof (gw.body.genomicContexts[i].gene.ensemblGeneIds[0]) !== 'undefined') {
-            geneList.push(gw.body.genomicContexts[i].gene.ensemblGeneIds[0].ensemblGeneId)
-        }
-        else {
-            geneList.push('NAN')
-        }
-    }   
+  if (api === 'gwas_api') {
+    // Return some information about the API (just for testing)
+    // const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api') -> Bonne addresse: ebi.ac.uk/gwas/beta/rest/api
+    const data = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api/singleNucleotidePolymorphisms/rs6538678').set('Accept', 'application/json')
+    res.render('test', {
+      data: JSON.stringify(data.body),
+      name: 'GWAS'
+    })
+  }
+  if (api === 'gtex_api') {
+    // Return the version of the api (just for testing) faire un attention à la version (soit 37, 38)!
+    // const data = await superagent.get('grch37.rest.ensembl.org/variation/human/rs4646963').set('Accept', 'application/json')
+    const data = await superagent.get('rest.ensembl.org/variation/human/rs6538678').set('Accept', 'application/json')
     let result = {
-        name: gt.body.name
-        , location: gt.body.mappings[0].location
-        , start: gt.body.mappings[0].start
-        , end: gt.body.mappings[0].end
-        , seq: gt.body.mappings[0].seq_region_name
-        , allele: gt.body.mappings[0].allele_string
-        , variant: gt.body.synonyms
-        , genesName: geneNameList
-        , genes: geneList
-        , assoSnp: asso.snp
-        , assopValue: asso.pvalue
+      name: data.body.name, // gene: geneId.body.genomicContext
+      location: data.body.mappings[0].location,
+      start: data.body.mappings[0].start,
+      end: data.body.mappings[0].end,
+      seq: data.body.mappings[0].seq_region_name,
+      allele: data.body.mappings[0].allele_string,
+      variant: data.body.synonyms
     }
     res.render('result', {
-        data: result        
+      data: result,
+      name: 'GTEX'
     })
+  }
+}
+// Reseach function with RS id given by the user
+async function searchAssociationsByTraits(trait, res) {
+  const gw = await superagent.get('https://www.ebi.ac.uk/gwas/beta/rest/api/associations/search/findByEfoTrait?efoTrait=' + trait).set('Accept', 'application/json')
+  let asso = {}
+  let reportedGeneList = []
+  let mappedGeneList = []
+  //boucle useless pour l'instant
+  for (let i in gw.body._embedded.associations) {
+    asso.snp = gw.body._embedded.associations[i].loci[0].strongestRiskAlleles[0].riskAlleleName
+    asso.pvalue = gw.body._embedded.associations[i].pvalue
+  }
+  let result = {
+    snp: asso.snp,
+    pvalue: asso.pvalue,
+    /* location: gw.body.mappings[0].location,
+              region: gw.body.mappings[0].seq_region_name,
+              reportedGenes: reportedGeneList,
+              mappedGenes: mappedGeneList */
+  }
+  res.render('association', {
+    dataAsso: result
+  })
+}
+async function searchRs(rs, res) {
+  const gt = await superagent.get(gtex + rs).set('Accept', 'application/json')
+  const gw = await superagent.get(gwas + rs).set('Accept', 'application/json')
+  const gwAsso = await superagent.get('www.ebi.ac.uk/gwas/beta/rest/api/associations/search/findByRsId?rsId=' + rs).set('Accept', 'application/json')
+  let geneList = []
+  let geneNameList = []
+  let asso = {}
+  for (let i in gwAsso.body._embedded.associations) {
+    asso.snp = gwAsso.body._embedded.associations[i].loci[0].strongestRiskAlleles[0].riskAlleleName
+    asso.pvalue = gwAsso.body._embedded.associations[i].pvalue
+  }
+  for (let i in gw.body.genomicContexts) {
+    if (typeof (gw.body.genomicContexts[i].gene.geneName) !== 'undefined') {
+      geneNameList.push(gw.body.genomicContexts[i].gene.geneName)
+    }
+    if (typeof (gw.body.genomicContexts[i].gene.ensemblGeneIds[0]) !== 'undefined') {
+      geneList.push(gw.body.genomicContexts[i].gene.ensemblGeneIds[0].ensemblGeneId)
+    } else {
+      geneList.push('NAN')
+    }
+  }
+  let result = {
+    name: gt.body.name,
+    location: gt.body.mappings[0].location,
+    start: gt.body.mappings[0].start,
+    end: gt.body.mappings[0].end,
+    seq: gt.body.mappings[0].seq_region_name,
+    allele: gt.body.mappings[0].allele_string,
+    variant: gt.body.synonyms,
+    genesName: geneNameList,
+    genes: geneList,
+    assoSnp: asso.snp,
+    assopValue: asso.pvalue
+  }
+  res.render('result', {
+    data: result
+  })
 }
 module.exports.testApi = testApi
 module.exports.searchRs = searchRs
+module.exports.searchAssociationsByTraits = searchAssociationsByTraits

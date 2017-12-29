bcb1522e84e2c03979bafc417b33168fb030e2e9
silouane <silouane.galinou@efrei.net>
2017-12-29 02:51:19 +0100

    test NCBI API

--- server/routes/snpInfo.js
+++ server/routes/snpInfo.js
@@ -15,8 +15,28 @@ router.post('/', (req, res) => {
   if (req.body.rs) {
     console.log("test route" + req.body.rs)
     searchRs(req.body.rs, res)
+    searchRSNcbi(req.body.rs, res)
   }
 })
+//test ncbi API
+async function searchRSNcbi(rs, res) {
+  let rsId = rs.substr(2, rs.length)
+  const ncbi = await superagent.get('https://api.ncbi.nlm.nih.gov/variation/v0/beta/refsnp/' + rsId).set('Accept', 'application/json')
+  console.log('test ncbi_snp' + ncbi.body.refsnp_id)
+  let previousMergeList = []
+  for (let i in ncbi.body.dbsnp1_merges) {
+    previousMergeList.push('rs' + ncbi.body.dbsnp1_merges[i].merged_rsid)
+  }
+  let result = {
+    name: 'rs' + ncbi.body.refsnp_id,
+    previousMerge: previousMergeList
+  }
+  console.log('test result' + result.name + result.previousMerge[0])
+  /*
+  res.render('snpInfo', {
+    data: result
+  })*/
+}
 async function searchRs(rs, res) {
   const gt = await superagent.get(gtex + rs).set('Accept', 'application/json')
   const gw = await superagent.get(gwas + rs).set('Accept', 'application/json')

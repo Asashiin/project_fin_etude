45256fda5a47d3eaeb400195d35c873a725006d2
Asashiin <xavier.payet@efrei.net>
2018-01-27 19:36:49 +0100

    working on gtex api

--- server/routes/snpInfo.js
+++ server/routes/snpInfo.js
@@ -1,25 +1,24 @@
 var express = require('express')
 const jsonFunc = require('jsonFunc')
 var router = express.Router()
-var excel = require('excel4node');
+// var excel = require('excel4node');
 router.get('/', (req, res) => {
   // res.render('result')
 })
 router.post('/', (req, res) => {
-  console.log('test route')
+  // console.log('test route')
   if (req.body.rs) {
-    console.log('test route' + req.body.rs)
+    // console.log('test route' + req.body.rs)
     jsonFunc.searchRs(req.body.rs, res)
     jsonFunc.searchRSNcbi(req.body.rs, res)
   }
 })
-/*
-router.get('/dlExcel', (req, res) => {
+/* router.get('/dlExcel', (req, res) => {
   console.log(req.test + '1')
   console.log("test export Excel")
-})*/
+}) */
 router.post('/dlExcel', (req, res) => {
   console.log(req.data)
-  console.log("test export Excel")
+  console.log('test export Excel')
 })
 module.exports = router
--- server/routes/testAPIEnsembl.js
+++ server/routes/testAPIEnsembl.js
@@ -1,25 +1,25 @@
 var express = require('express')
 const jsonFunc = require('jsonFunc')
 const excel = require('rwExcel')
-let path = require('path')
-let filePath = path.join('~/SNP.txt')
+// let path = require('path')
+// let filePath = path.join('~/SNP.txt')
 var router = express.Router()
-/*router.get('/', (req, res) => {
+/* router.get('/', (req, res) => {
   res.render('testAPIEnsembl')
-})*/
+}) */
 router.post('/', (req, res) => {
   console.log('test route')
   let datal = []
   let listRS = []
   if (req.body.rs) {
-    //console.log('test route' + req.body.rs)
+    // console.log('test route' + req.body.rs)
     // POUR l'instant ca lit juste un rs, le rs qu'on rentre dans la recherche
     // faudra passer ca sur un fonction sur le bouton que j'ai mis a la page APIEnsembl
     jsonFunc.APIEnsembl(req.body.rs).then(result => {
       res.render('testAPIEnsembl', {
         data: result
       })
-      console.log(result.name);
+      console.log(result.name)
       datal.push(result)
       console.log('test' + JSON.stringify(datal))
       excel.writeFichier(JSON.stringify(datal))

6149ff3368e7f955b7afed880975b24579e51404
silouane <silouane.galinou@efrei.net>
2018-02-03 11:33:48 +0100

    save

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const bodyParser = require('body-parser')
-// const route = require('./routes/route')
 const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({
   extended: true

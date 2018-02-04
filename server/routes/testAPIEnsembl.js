526ad065230ead22cf989b68466c61707ae7a022
silouane <silouane.galinou@efrei.net>
2018-02-04 03:35:58 +0100

    lecture depus le input form OK

--- server/routes/testAPIEnsembl.js
+++ server/routes/testAPIEnsembl.js
@@ -1,7 +1,10 @@
 var express = require('express')
 const jsonFunc = require('jsonFunc')
 const excel = require('rwExcel')
+var fs = require('fs')
 var router = express.Router()
+var formidable = require('formidable')
+/*****/
 router.post('/', (req, res) => {
   console.log('test route')
   if (req.body.rs) {
@@ -17,7 +20,20 @@ router.post('/', (req, res) => {
     })
   }
 })
+/******/
 router.post('/fileup', (req, res) => {
-  console.log('test route fileup')
+  var form = new formidable.IncomingForm()
+  form.parse(req, function (err, fields, files) {
+    console.log(files.fileToUpload.path)
+    var pth = files.fileToUpload.path;
+    //déplacer fichier si on veut le save sur le serveur/qqc part -->>dossier a lire
+    /* var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
+    fs.rename(pth, newpath, function (err) {
+      if (err) throw err;
+      res.write('File uploaded and moved!');
+    })*/
+    excel.readFichier(pth)
+    // --> a voir sin crrée directement l'appel a l'écriture des résultats en asynchrones ici ou à un autre endroit
+  })
 })
 module.exports = router

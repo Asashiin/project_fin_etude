355b02aac446b2c4f81e5b252a88504f4757fce7
Asashiin <xavier.payet@efrei.net>
2018-02-07 17:19:56 +0100

    Fix writing in file txt

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,15 +11,31 @@ router.get('/', (req, res) => {
 router.post('/fileup', (req, res) => {
   let form = new formidable.IncomingForm()
   form.parse(req, (err, fields, files) => {
+    if (err) throw err
     let pth = files.fileToUpload.path
     let data = fs.readFileSync(pth, {
       encoding: 'utf-8'
     })
     let listId = data.split('\n')
-    for (let i = 0; i < listId.length; i++) {
+    jsonFunc.APIEnsembl(listId).then(result => {
+      let wPath = 'public/snpi.txt'
+      // stream
+      let wstream = fs.createWriteStream(wPath)
+      for (let i = 0; i < result.length; i++) {
+        let test = JSON.stringify(result[i])
+        wstream.write(test)
+        wstream.write('\n')
+      }
+      wstream.end()
+      // setTimeout(function () {}, 1000)
+      res.render('testAPIEnsembl', {
+        data: result
+      })
+    })
+    /* for (let i = 0; i < listId.length; i++) {
       jsonFunc.APIEnsembl(listId[i]).then(result => {
         console.log(result)
-        let test = JSON.stringify(buffer)
+        let test = JSON.stringify(result)
         let wPath = 'public/snpi.txt'
         /* fs.writeFile(wPath, test, 'utf-8', (err) => {
            console.log('test appendfile 1 ')
@@ -29,24 +45,22 @@ router.post('/fileup', (req, res) => {
              console.log('succeed at :  ' + wPath)
            }
          })
-      
-      fs.appendFile(wPath, test, 'utf-8', (err) => {
+         fs.appendFile(wPath, test, 'utf-8', (err) => {
           console.log('test appendfile 1 ')
           if (err) {
             console.log('err writefile : ' + err)
           } else {
             console.log('succeed at :  ' + wPath)
           }
-        })*/
-
-        //stream
-        let wstream = fs.createWriteStream(wPath);
+        })
+        // stream
+        let wstream = fs.createWriteStream(wPath)
         wstream.write(test)
         wstream.end()
         setTimeout(function () {}, 1000)
       })
-    }
+    } */
   })
-  res.render('index')
+  // res.render('index')
 })
 module.exports = router

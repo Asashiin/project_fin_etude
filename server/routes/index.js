50a5e045f11aa34fb8d6dedb7d60fed2d0952118
silouane <silouane.galinou@efrei.net>
2018-02-09 03:52:03 +0100

    fix, rajout route location pas de rendu pour linstant

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,9 @@
 var express = require('express')
 var router = express.Router()
 const jsonFunc = require('jsonFunc')
-// const excel = require('rwExcel')
 var fs = require('fs')
 var formidable = require('formidable')
-/**********/
+  /**********/
 router.get('/', (req, res) => {
   res.render('index')
 })
@@ -23,49 +22,21 @@ router.post('/fileup', (req, res) => {
         res.render('index')
       }
       let wPath = 'public/snpi.txt'
-      // stream
+        // stream
       let wstream = fs.createWriteStream(wPath)
       for (let i = 0; i < result.length; i++) {
         let test = JSON.stringify(result[i])
         wstream.write(test)
         wstream.write('\n')
         wstream.write('\n')
+        wstream.write('\n')
       }
       wstream.end()
-      // setTimeout(function () {}, 1000)
+        // setTimeout(function () {}, 1000)
       res.render('testAPIEnsembl', {
         data: result
       })
     })
-    /* for (let i = 0; i < listId.length; i++) {
-      jsonFunc.APIEnsembl(listId[i]).then(result => {
-        console.log(result)
-        let test = JSON.stringify(result)
-        let wPath = 'public/snpi.txt'
-        /* fs.writeFile(wPath, test, 'utf-8', (err) => {
-           console.log('test appendfile 1 ')
-           if (err) {
-             console.log('err writefile : ' + err)
-           } else {
-             console.log('succeed at :  ' + wPath)
-           }
-         })
-         fs.appendFile(wPath, test, 'utf-8', (err) => {
-          console.log('test appendfile 1 ')
-          if (err) {
-            console.log('err writefile : ' + err)
-          } else {
-            console.log('succeed at :  ' + wPath)
-          }
-        })
-        // stream
-        let wstream = fs.createWriteStream(wPath)
-        wstream.write(test)
-        wstream.end()
-        setTimeout(function () {}, 1000)
-      })
-    } */
   })
-  // res.render('index')
 })
 module.exports = router
--- server/routes/location.js
+++ server/routes/location.js
@@ -1,13 +1,23 @@
 var express = require('express')
 var router = express.Router()
 const jsonFunc = require('jsonFunc')
+/*****/
 router.post('/', (req, res) => {
-  if (req.body.location) {
-    console.log('test route, location : ' + req.body.location)
-    jsonFunc.searchSnpByLocation(req.body.location, res)
-  }
-})
-router.get('/', (req, res) => {
-  //  res.render('index')
+  /*****/
+  let data = req.body.location
+  let max = req.body.location_nbMax
+  console.log('nb_max' + req.body.location_nbMax)
+  let listLoc = data.split('/')
+  jsonFunc.searchSnpByLocation(listLoc, max).then(result => {
+    if (typeof result === 'string') {
+      console.log(result)
+      res.render('index')
+    } else {
+      console.log(result)
+      res.render('location', {
+        data: result
+      })
+    }
+  })
 })
 module.exports = router

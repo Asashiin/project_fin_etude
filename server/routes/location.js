b083166115039a23aded76bfaf148d35130d40be
Asashiin <xavier.payet@efrei.net>
2018-02-15 15:15:09 +0100

    Location works

--- server/routes/location.js
+++ server/routes/location.js
@@ -8,15 +8,14 @@ router.post('/', (req, res) => {
   let max = req.body.location_nbMax
   let pop = req.body.population
   let size = req.body.size
-  console.log('nb_max : ' + req.body.location_nbMax)
   let listLoc = data.split('/')
-  jsonFunc.searchSnpByLocation(listLoc, pop, size).then(result => {
+  jsonFunc.searchSnpByLocation(listLoc, pop, size, max).then(result => {
     if (typeof result === 'string') {
       console.log(result)
       res.render('index')
     } else {
-      console.log(result)
-      console.log(JSON.stringify(result[0].snp[0]))
+      // console.log(result)
+      // console.log(JSON.stringify(result[0].snp[0]))
       res.render('location', {
         data: result,
         nbMaxi: max

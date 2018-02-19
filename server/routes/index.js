d5e5bdacdbf79d69dbd6d9e81e711dead0257803
Asashiin <xavier.payet@efrei.net>
2018-02-19 18:09:57 +0100

    Working on file

--- server/routes/index.js
+++ server/routes/index.js
@@ -47,12 +47,12 @@ router.post('/fileup', (req, res) => {
       encoding: 'utf-8'
     })
     let listId = data.split('\n')
-    jsonFunc.APIEnsembl(listId).then(result => {
+    jsonFunc.APIEnsembl(listId, fields.population, fields.size).then(result => {
       if (typeof result === 'string') {
         console.log(result)
         // res.render('index')
       }
-      let wPath = 'public/snpi.txt'
+      let wPath = 'public/SNPInfo.txt'
       // stream
       let wstream = fs.createWriteStream(wPath)
       for (let i = 0; i < result.length; i++) {
